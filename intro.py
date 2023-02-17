a35f8e1dd1959bb7810f7b7e844436bcbe3a7522
zalogarciam <gegarciam95@gmail.com>
2023-02-16 22:14:46 -0500

    Refactor code

--- intro.py
+++ intro.py
@@ -1,16 +1,6 @@
-def sum(a, b): return int(a) + int(b)
-def subs(a, b): return int(a) - int(b)
-def mult(a, b): return int(a) * int(b)
-def div(a, b): return int(a) / int(b)
-def calc(opt, a, b):
-    if opt == '+': return sum(a, b)
-    elif opt == '-': return subs(a, b)
-    elif opt == '*': return mult(a, b)
-    elif opt == '/': return div(a, b)
-    else: return "wrong operation"
-
 a = input('a: ')
 b = input('b: ')
 opt = input('operation: ')
-print(str(calc(opt, a, b)))
+print(str(eval(a + opt + b)))
+
 
