a8d5ca2916e16ab8b7ee10140af1488ec3ecf5ad
zalogarciam <gegarciam95@gmail.com>
2023-02-26 15:45:31 -0500

    Homework updated

--- sql/exercise.sql
+++ sql/exercise.sql
@@ -38,7 +38,6 @@ CREATE TABLE direcciones(
 
 -- DDL (ALTER)
 
-
 -- TAREA
 -- PASOS: Primero ingresar los datos y luego resolver las siguientes queries:
 -- DIRECCION            NUM     REFERENCIA              ALUMNO_ID
@@ -53,13 +52,35 @@ CREATE TABLE direcciones(
 'Alameda Chabuca', 740, 'Dos cuadras de la piscina', 6
 'Callejon Bravo', 14, 'A dos casas de la reja', 3
 
+insert into direcciones values(default, 'Calle los Girasoles', 750, 'Al costado de la polleria', 1);
+insert into direcciones values(default,'Calle Los aviadores', 1050, NULL,2);
+insert into direcciones values(default,'Av El Sol', 125, 'Del ovalo a media cuadra', 1);
+insert into direcciones values(default,'Av Los Gallos', 777, NULL, 3);
+insert into direcciones values(default,'Av Tupac Yupanqui', 123, NULL, 7);
+insert into direcciones values(default,'Av Siempre viva', 7840, 'Al frente de la ferreteria', 8);
+insert into direcciones values(default,'Calle Los martires', 6520, NULL, 5);
+insert into direcciones values(default,'Pasaje de las flores', 526, NULL, 4);
+insert into direcciones values(default,'Alameda Chabuca', 740, 'Dos cuadras de la piscina', 6);
+insert into direcciones values(default,'Callejon Bravo', 14, 'A dos casas de la reja', 3);
 
 -- 1. Buscar todas las direcciones que sean calles
+select * from direcciones where direccion like '%Calle%';
+
 -- 2. Listar todas las direcciones sin referencia
+select * from direcciones where referencia is null;
+
 -- 3. Listar todas las direcciones que sean menores que 1000
+select * from direcciones where numero < 1000;
+
 -- 4. Listar todas las direcciones que sean o Av o Pasaje
+select * from direcciones where direccion like '%Av%' or direccion like '%Pasaje%';
+
 -- 5. Listar todas las direcciones de los alumnos 1 o que vivan en calles o que no tengan referencias
+select * from direcciones where alumno_id = 1 or direccion like '%Calle%' or referencia is null
+
 -- 6. Listar todas las direcciones que sean calle y que su referencia no sea nula y que su alumno sea el 1
+select * from direcciones where alumno_id = 1 and direccion like '%Calle%' and referencia is not null
+
 
 
 -- Para borrar una tabla
