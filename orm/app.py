a4551cf4221790cea6e3672d305abdbe535de53a
zalogarciam <gegarciam95@gmail.com>
2023-03-02 21:40:30 -0500

    Started with orm flask

--- orm/app.py
+++ orm/app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+from os import environ
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
+conn = SQLAlchemy(app = app)
 
 if __name__ == '__main__':
     app.run(debug=True)
