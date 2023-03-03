df2cab348ffe8e7e94c08a54486ac29afa032df5
zalogarciam <gegarciam95@gmail.com>
2023-03-02 23:04:13 -0500

    orm model implementation

--- orm/app.py
+++ orm/app.py
@@ -2,12 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 from os import environ
+from bd import conn
 
 load_dotenv()
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
-conn = SQLAlchemy(app = app)
+
+
+conn.init_app(app);
+# conn = SQLAlchemy(app = app)
 
 if __name__ == '__main__':
     app.run(debug=True)
