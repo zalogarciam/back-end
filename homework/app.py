58bca91b17310b9c131357522f11bd8e20982526
zalogarciam <gegarciam95@gmail.com>
2023-03-09 23:00:29 -0500

    Add api call in app.py

--- homework/app.py
+++ homework/app.py
@@ -1,11 +1,22 @@
 from flask import Flask
 from flask_migrate import Migrate
 from bd import connection
+from dotenv import load_dotenv
+from os import environ
+from controllers.usuario_controller import UsuariosController
+from flask_restful import Api
+
+load_dotenv() 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
+api = Api(app)
+
 connection.init_app(app)
 
 Migrate(app = app, db = connection)
+
+api.add_resource(UsuariosController, '/registro')
 
 if __name__ == '__main__':
     app.run(debug=True)
