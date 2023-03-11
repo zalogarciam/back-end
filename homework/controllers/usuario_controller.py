3e47b359d19076a2ab35a0d7e8d5029d3ed76add
zalogarciam <gegarciam95@gmail.com>
2023-03-10 19:57:33 -0500

    Added missing message when saving user

--- homework/controllers/usuario_controller.py
+++ homework/controllers/usuario_controller.py
@@ -1,7 +1,8 @@
 from flask_restful import Resource, request
 from dtos.usuario_dto import UsuarioDto
-from homework.models.usuario_model import Usuario
+from models.usuario_model import Usuario
 from bcrypt import hashpw, gensalt
+from bd import connection
 
 class UsuariosController(Resource):
     def post(self):
@@ -9,6 +10,7 @@ class UsuariosController(Resource):
         dto = UsuarioDto()
         try:
             data_valid = dto.load(data)
+            print(data)
             salt = gensalt(rounds = 10)
             password = bytes(data_valid.get("password"), "utf-8")
             password_hashed = hashpw(password, salt)
@@ -16,10 +18,14 @@ class UsuariosController(Resource):
 
             usuario = Usuario(correo = data_valid.get('correo'),
                               password = password_hashed_str,
-                              nombre = data_valid.get('nombre'),
+                              nombres = data_valid.get('nombres'),
                               apellido = data_valid.get('apellido')
                               )
-
+            connection.session.add(usuario)
+            connection.session.commit()
+            return {
+                'message': 'Usuario creado exitosamente'
+            }
         except Exception as error:
             return {
                 'message': 'Error al ingresar el usuario',
