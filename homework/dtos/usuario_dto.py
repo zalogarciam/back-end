718f709e6f990b1317fd3b63093bb8a3decf94fe
zalogarciam <gegarciam95@gmail.com>
2023-03-10 22:25:22 -0500

    Remove password when returning data

--- homework/dtos/usuario_dto.py
+++ homework/dtos/usuario_dto.py
@@ -1,8 +1,9 @@
-from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema, auto_field
 from marshmallow import Schema, fields
 from models.usuario_model import Usuario
 
 class UsuarioDto(SQLAlchemyAutoSchema):
+    password = auto_field(load_only = True)
     class Meta:
         model = Usuario
 
