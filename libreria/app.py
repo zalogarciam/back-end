cac789e6bedd027f2842ce03d5f4ba164c05726c
zalogarciam <gegarciam95@gmail.com>
2023-03-15 22:54:48 -0500

    Fix references

--- libreria/app.py
+++ libreria/app.py
@@ -5,21 +5,21 @@ from flask_migrate import Migrate
 from flask_restful import Api
 
 from db import connection
+from controllers.usuario_controller import RegistroController
 
 
 load_dotenv() 
 
 app = Flask(__name__)
-
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
-
 flask_api = Api(app=app)
-
 
 connection.init_app(app)
 
 
 Migrate(app=app, db=connection)
+
+flask_api.add_resource(RegistroController, '/registro')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- libreria/controllers/usuario_controller.py
+++ libreria/controllers/usuario_controller.py
@@ -2,7 +2,7 @@ from bcrypt import gensalt, hashpw
 from flask_restful import Resource, request
 from dtos.usuario_dto import UsuarioDto
 from db import connection
-from homework.models.usuario_model import Usuario
+from models.usuario_model import Usuario
 
 class RegistroController(Resource):
     def post(self):
@@ -15,7 +15,10 @@ class RegistroController(Resource):
             password = bytes(data_serialized.get('password'), 'utf-8')
             hashed_password = hashpw(password, salt).decode('utf-8')
         
+            data_serialized['password'] = hashed_password
             user = Usuario(**data_serialized)
+            connection.session.add(user)
+            connection.session.commit()
             return {
                 'message': 'Usuario creado',
             }
