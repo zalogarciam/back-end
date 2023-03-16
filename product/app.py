fca64a6e5384d07d2debf844c535d4c3d9b11bf3
zalogarciam <gegarciam95@gmail.com>
2023-03-15 21:36:34 -0500

    Add db and connection to app

--- product/app.py
+++ product/app.py
@@ -1,1 +1,25 @@
 from flask import Flask
+from dotenv import load_dotenv
+from os import environ
+from flask_migrate import Migrate
+from flask_restful import Api
+
+from bd import connection
+
+
+load_dotenv() 
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
+
+flask_api = Api(app=app)
+
+
+connection.init_app(app)
+
+
+Migrate(app=app, db=conn)
+
+if __name__ == '__main__':
+    app.run(debug=True)
